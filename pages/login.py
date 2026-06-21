063a7510ebafd5ae135ef663f83b8f134369809f
KietIT-bip-bip <dangquockiet1501.tt@gmail.com>
2026-06-21 16:02:02 +0700

    B12

--- pages/login.py
+++ pages/login.py
@@ -15,17 +15,70 @@ class LoginPage(QMainWindow):
         self.root_dir = root_dir
 
         # load file ui
-        ui_path = self.root_dir + "/GUI/lolgin.ui"
+        ui_path = self.root_dir + "/ui/login.ui"
         uic.loadUi(ui_path, self)
 
         # bat su kien cho cac nut bam
-        # TODO
+        # 1. nut login
+        self.login.clicked.connect(
+            self.handle_login
+        )  # click vao nut login -> goi ham handle_login
+        # 2. nut chuyen register
+        self.nav_signup.clicked.connect(
+            self.goto_signup
+        )  # click vao nut chuyen signup -> goi ham goto_registergoto_signup
+
 
         # chay app
         self.show()
 
     # ------------------ xu ly su kien ------------------
-    # TODO
+    def handle_login(self):
+        # lay du lieu tu input form
+        email_input = (
+            self.email.text().strip()
+        )  # lay du lieu tu username input, xoa khoang trang 2 dau
+        password_input = self.password.text()
+
+        # validate du lieu
+        if self.__validate_input(email_input, password_input) is not None:
+            print(self.__validate_input(email_input, password_input))
+            # co loi -> bao loi
+            self.__show_message(self.__validate_input(email_input, password_input))
+            return  # khong lam gi nua
+        else:
+            # thanh cong -> chuyen sang home
+            self.__goto_home()
+
+    def goto_signup(self):
+        from pages.signup import SignupPage
+
+        self.signup_page = SignupPage(
+            main_window=self.main_window, root_dir=self.root_dir
+        )
+        self.close()  # ✅ đóng cửa sổ
+    def __goto_home(self):
+        from pages.home import HomePage
+
+        self.home_page = HomePage(main_window=self.main_window, root_dir=self.root_dir, cur_acc=account)
+        self.close()  # ✅ đóng cửa sổ
+
+    def __validate_input(self, email, password):
+        # kiem tra email
+        regex = r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
+        if re.fullmatch(regex, email) is None:
+            return "Email khong hop le!"
+
+        # kiem tra password
+        if len(password) < 6:
+            return "Password phai tu 6 chu so tro len!"
+
+        # kiem tra khop tai khoan (mock data)
+        if email != account["email"] or password != account["password"]:
+            return "Email hoac password khong chinh xac!"
+
+        return None  # khong co loi
+    
     # ------------------ ham ho tro ------------------
     def __show_message(self, message):
         # Khởi tạo hộp thoại thông báo
--- pages/signup.py
+++ pages/signup.py
@@ -15,17 +15,76 @@ class SignupPage(QMainWindow):
         self.root_dir = root_dir
 
         # load file ui
-        ui_path = self.root_dir + "/GUI/signup.ui"
+        ui_path = self.root_dir + "/ui/signup.ui"
         uic.loadUi(ui_path, self)
 
         # bat su kien cho cac nut bam
-        # TODO
+         # 1. nut login
+        self.signup.clicked.connect(
+            self.handle_register
+        )  # click vao nut login -> goi ham handle_register
+        # 2. nut chuyen register
+        self.nav_login.clicked.connect(
+            self.goto_login
+        )  # click vao nut chuyen register -> goi ham goto_login
 
         # chay app
         self.show()
 
     # ------------------ xu ly su kien ------------------
-    # TODO
+    def handle_register(self):
+        # lay du lieu tu input form
+        email_input = (
+            self.email.text().strip()
+        )  # lay du lieu tu email input, xoa khoang trang 2 dau
+        password_input = self.password.text()
+        fullname_input = self.full_name.text()
+
+        # kiem tra fullname
+        if fullname_input.strip() == "":
+            self.__show_message("Vui lòng nhập đầy đủ họ tên!")
+            return  # bao loi -> ket thuc
+
+        # validate du lieu
+        if self.__validate_input(email_input, password_input) is not None:
+            print(self.__validate_input(email_input, password_input))
+            # co loi -> bao loi
+            self.__show_message(self.__validate_input(email_input, password_input))
+            return  # khong lam gi nua
+        else:
+            # luu tai khoan
+            account["fullname"] = fullname_input
+            account["email"] = email_input
+            account["password"] = password_input
+            # thanh cong -> chuyen sang home
+            self.__goto_home()
+
+    def goto_login(self):
+        from pages.login import LoginPage
+
+        self.login_page = LoginPage(
+            main_window=self.main_window, root_dir=self.root_dir
+        )
+        self.close()  # ✅ đóng cửa sổ
+    def __goto_home(self):
+        from pages.home import HomePage
+
+        self.home_page = HomePage(
+            main_window=self.main_window, root_dir=self.root_dir, cur_acc=account
+        )
+        self.close()  # ✅ đóng cửa sổ
+
+    def __validate_input(self, email, password):
+        # kiem tra email
+        regex = r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
+        if re.fullmatch(regex, email) is None:
+            return "Email khong hop le!"
+
+        # kiem tra password
+        if len(password) < 6:
+            return "Password phai tu 6 chu so tro len!"
+
+        return None  # khong co loi
     # ------------------ ham ho tro ------------------
     def __show_message(self, message):
         # Khởi tạo hộp thoại thông báo
